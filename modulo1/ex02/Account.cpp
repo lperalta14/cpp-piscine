25cf545d39e5f0e4fe9d58346931b2e405fd7594
lperalta <lperalta@c2r6s4.42malaga.com>
2026-06-04 13:43:34 +0200

    terminado modulo completo

--- modulo1/ex02/Account.cpp
+++ modulo1/ex02/Account.cpp
@@ -17,6 +17,11 @@ int Account::getTotalAmount()
 	return _totalAmount;
 }
 
+int	Account::checkAmount( void ) const
+{
+	return _amount;
+}
+
 int Account::getNbDeposits()
 {
 	return _totalNbDeposits;
@@ -41,7 +46,7 @@ void	Account::_displayTimestamp()
 	char buffer[20];
 	time_t actualtime = std::time(NULL);
 	strftime(buffer, 20, "%Y%m%d_%H%M%S", std::localtime(&actualtime));
-	std::cout << "[" << buffer << "]";
+	std::cout << "[" << buffer << "] ";
 }
 
 void	Account::displayAccountsInfos( void )
@@ -56,6 +61,7 @@ void	Account::makeDeposit(int deposit)
 	_amount += deposit;
 	_nbDeposits ++;
 	_totalNbDeposits ++;
+	_totalAmount += deposit;
 	_displayTimestamp();
 	std::cout << "index:" << _accountIndex << ";p_amount:" << p_amount << ";deposit:" << deposit << ";amount:" << _amount << ";nb_deposits:" << _nbDeposits << std::endl;
 }
@@ -79,6 +85,12 @@ bool Account::makeWithdrawal(int withdrawal)
 		std::cout << "index:" << _accountIndex << ";p_amount:" << p_amount << ";withdrawal:" << withdrawal << ";amount:" << _amount << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
 	}
 	return (true);
+}
+
+void Account::displayStatus( void ) const
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
 }
 
 Account::~Account(){
