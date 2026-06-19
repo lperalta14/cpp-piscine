0a719e245796c35baff3fe03cd5d221985a89339
lperalta <lperalta@student.42malaga.fr>
2026-06-19 12:59:36 +0200

    cpp terminado

---
+++ modulo01/ex01/Zombie.cpp
@@ -0,0 +1,13 @@
+#include "Zombie.h"
+
+Zombie::Zombie(std::string name) : _name(name) {}
+
+void	Zombie::announce( void )
+{
+	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << _name <<": me remorí boludo" << std::endl;
+}
---
+++ modulo01/ex01/Zombie.hpp
@@ -0,0 +1,15 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+# include "Zombie.h"
+
+class Zombie
+{
+private:
+	std::string	_name;
+public:
+	Zombie(std::string name);
+	~Zombie();
+	void	announce(void);
+};
+
+#endif
