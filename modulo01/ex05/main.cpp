9df86551d50661b6852973696760b4f634e488ab
yogalinux <luisperalta@gmail.com>
2026-07-06 12:23:42 +0200

    Harl did

---
+++ modulo01/ex05/main.cpp
@@ -0,0 +1,13 @@
+#include "Harl.hpp"
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "usage: ./Harl \"level\"" << std::endl;
+		return (1); 
+	}
+	Harl harl;
+
+	harl.complain(argv[1]);
+}
