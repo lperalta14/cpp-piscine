cd255f588263792b716c7c63acc38738fae3bce5
yogalinux <luisperalta@gmail.com>
2026-09-01 22:08:44 +0200

    wip: Form.hpp complete, need complete .cpp

--- modulo05/ex01/Form.hpp
+++ modulo05/ex01/Form.hpp
@@ -1,15 +1,39 @@
 #ifndef FORM_HPP
 # define FORM_HPP
 # include <string>
+# include <iostream>
 
 class Form
 {
 private:
-    const std::string   _name;
-    bool                _signed;
-    const int           _gradesign;
-    const int           _gradeexe;
+	const std::string   _name;
+	bool                _signed;
+	const int           _gradeSign;
+	const int           _gradeExe;
 public:
+	Form();
+	Form(const std::string& name, int gradeSign, int gradeExe);
+	Form(const Form& other);
+	Form& operator=(const Form& other);
+	~Form();
+
+	std::string	getName() const;
+	bool		getSigned() const;
+	int			getGradeSign() const;
+	int			getGradeExe() const;
+
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		virtual const char* what() const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		virtual const char* what() const throw();
+	};
 };
+
+std::ostream& operator<<(std::ostream& out, const Form& form);
 
 #endif
